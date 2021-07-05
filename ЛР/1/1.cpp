887c031a7a5f91f02348a34b62d9bffa7b4bed77
Jetris02 <86570861+Jetris02@users.noreply.github.com>
2021-07-05 11:33:37 +0300

    Add files via upload

---
+++ ЛР/1/1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void set(int* a, int size)
+{
+	cout << "Введите элементы массива\n";
+	for (int i = 0; i < size; i++)
+		cin >> a[i];
+}
+
+void get(int* a, int size)
+{
+	cout << "\nМассив\n";
+	for (int i = 0; i < size; i++)
+		cout << a[i] << "  ";
+	cout << endl;
+}
+
+int* change(int* a, int& size)
+{
+	int* b;
+	int M = 0, j = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (a[i] >= 0)
+			M++;
+	}
+	b = new int[M];
+	for (int i = 0; i < size; i++)
+	{
+		if (a[i] >= 0)
+		{
+			b[j] = a[i];
+			j++;
+		}
+	}
+	size = M;
+	a = b;
+	return a;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int* array;
+	int N;
+	cout << "Введите количество элементов в массиве\n";
+	cin >> N;
+	array = new int[N];
+	set(array, N);
+	get(array, N);
+	array = change(array, N);
+	get(array, N);
+	system("pause");
+}
---
+++ ЛР/2/2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void vowels(string str)
+{
+	string s, vow = "AaEeIiOoUuYy";
+	for (int i = 0; i < str.length(); i++)
+	{
+		for (int j = 0; j < vow.length(); j++)
+		{
+			if (str[i] == vow[j])
+				s += str[i];
+		}
+	}
+	cout << "Гласные в строке\n";
+	cout << s << endl;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	string str;
+	cout << "Введите строку\n";
+	cin >> str;
+	vowels(str);
+	system("pause");
+}
